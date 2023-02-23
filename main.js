02f05c6423e2b1e51659cc08d14234add438b5b2
Otavio Bacchin <otavio.bacchin@gmail.com>
2023-02-23 16:09:31 -0300

    Bug Fixs com os títulos das colunas e manutenção do sistema de adicionar e deletar itens da agenda.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const form = document.getElementById('form-agenda')
 const imgDeletar = '<img src="./images/deletar.png" alt="Deletar">'
 const nomes = [] //Cria um array para os nomes
 const numeros = [] //Cria um array para os números
-
 var linhas = '' //var para adicionar uma linha a mais a cada nota/atividade adicionada
 
 form.addEventListener('submit', function(e) { //Cria um 'evento' para quando utilizarmos o form
@@ -10,9 +9,9 @@ form.addEventListener('submit', function(e) { //Cria um 'evento' para quando uti
 
     adicionaLinha() //Função para adicionar a linha através do formulário
     atualizaTabela() //Função para atualizar a tabela adicionando as linhas novas
-    atualizaMediaFinal() //Função para atualizar a média final
-
 })
+
+
 
 function adicionaLinha() {
     const inputNome = document.getElementById('nome-contato')
@@ -24,29 +23,55 @@ function adicionaLinha() {
     } else{
 
     nomes.push(inputNome.value)
-    numeros.push(parseFloat(inputNumero.value))
+    numeros.push(inputNumero.value)
 
-    var linha = '<tr>' //Cria uma linha nova
-    linha += `<td>${inputNome.value}</td>` //Adiciona o novo nome da atividade na coluna
-    linha += `<td>${inputNumero.value}</td>` //Adiciona a nova nota da atividade na coluna
-    linha += `<th><button class="deletar" onclick="removerElemento(event.target)"><img src="./images/deletar.png" alt=""></button></th>`
-    linha += '</tr>'
+    var linha = `<tr id='${inputNome.value}'><td>${inputNome.value}</td><td>${inputNumero.value}</td><td><button class="deletar" onclick="removerElemento(event.target)"><img src="./images/deletar.png" alt=""></button></td></tr>`
     
     linhas += linha
 
     inputNome.value = '' //Limpa os campos do formulário após o adicionar
     inputNumero.value = '' //Limpa os campos do formulário após o adicionar
+
+    console.log(linhas)
+    console.log(linha)
+    console.log(nomes)
+    console.log(numeros)
+
     }
 }
+
+
 
 function atualizaTabela() {
-    const corpoTabela = document.querySelector('tbody') //const para facilitar o a seleção da 'tbody'
-    corpoTabela.innerHTML = linhas //Comando pada ADICIONAR as linhas dentro da estrutura HTML
+    const corpoTabela = document.getElementById('tabela-corpo') //const para facilitar o a seleção da 'tbody'
+    corpoTabela.insertAdjacentHTML("beforeend", linhas) //Comando pada ADICIONAR as linhas dentro da estrutura HTML
+    linhas = ""
 }
 
+
 function removerElemento(elementoClicado) {
+
     elementoClicado.closest("tr").remove();
-    }
+    
+    // Encontre o nome que deseja remover
+    var nome = elementoClicado.closest("tr").querySelector("td:nth-child(1)").textContent;
+    var num = elementoClicado.closest("tr").querySelector("td:nth-child(2)").textContent;
+
+    // Encontre o índice do nome na array
+    var indice_nome = nomes.indexOf(nome);
+    var indice_num = numeros.indexOf(num);
+
+
+    // Remova o nome da array
+    nomes.splice(indice_nome, 1);
+    numeros.splice(indice_num, 1);
+
+    console.log(linhas)
+    console.log(nomes)
+    console.log(numeros)
+
+}
+
 
 function formatarTelefone(input) {
     var numero = input.value.replace(/\D/g, '');
